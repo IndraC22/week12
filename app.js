48006e4dd4c1394fe0b495102d6d20d7e9b93a22
IndraC22 <icalderon42727@gmail.com>
2024-11-22 19:03:50 -0600

    git.ignore, app.js, db.json, index.html. packcage-lock.json, package.json, style.css

---
+++ app.js
@@ -0,0 +1,60 @@
+
+$(document).ready(() => {
+    let articleList;
+
+    const articleInfoList = () => {
+        articleList.forEach(article => {
+            $('#content').append(`
+                    <div id="article ${article.id}" class="info-box">
+                         ID: ${article.id}, ${article.title}, ${article.author}
+                         <button id= ${article.id} class="btn btn-danger" >X</button>
+                    </div>
+                `)
+                $(`#${article.id}`).click(() => removeItem(article.id))
+        })
+    }
+
+    $.get('http://localhost:3000/articles', data => {
+        articleList = data
+    }).done(() => articleInfoList())
+
+
+    const removeItem = id => {
+        $.ajax({
+            url: `http://localhost:3000/articles/${id}`,
+            type: 'DELETE',
+        })
+    }
+
+    $('#myForm').submit(event => {
+        event.preventDefault()
+        const formData ={
+            title: $('#title').val(),
+            author: $('#author').val()
+        }
+        
+        $.post('http://localhost:3000/articles', formData, data =>{
+            alert(`data added: Title ${data.title}, Author: ${data.author}`)
+        })
+
+        $('#myForm').trigger('reset')
+    })
+
+    $('#myForm').submit(event => {
+        event.preventDefault()
+        const formData ={
+            id: $('#id').val(),
+            title: $('#title').val(),
+            author: $('#author').value(),
+        }
+
+        $.ajax({
+            url: `http://localhost:3000/articles/`,
+            type: 'PUT'
+            contentType: 'application/json',
+            data: JSON.stringify(formData)
+        })
+
+        $('myUpdateForm').trigger('reset')
+    })
+})
